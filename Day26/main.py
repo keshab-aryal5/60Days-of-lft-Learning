f6c6564f3d95c61a5734fecf1ed02a5f564d097e
keshab Aryal <keshabaryal152@gmail.com>
2024-06-26 20:45:05 +0545

    Done with day26

---
+++ Day26/main.py
@@ -0,0 +1,13 @@
+empire_url = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+import requests
+from bs4 import BeautifulSoup
+from html.parser import HTMLParser
+response = requests.get(url=empire_url)
+soup = BeautifulSoup(response.text,"html.parser")
+movie_list = soup.find_all("h3",class_="title")
+movie = [str(movie.getText()) for movie in movie_list]
+
+with open("movie.txt","w",encoding="UTF-8") as file:
+    for x in range(len(movie)-1,-1,-1):
+        file.write(movie[x])
+        file.write("\n")
