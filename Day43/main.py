95379576c4e356221cfdf94dd01f1e0901fefe5e
keshab Aryal <keshabaryal152@gmail.com>
2024-07-13 22:10:42 +0545

    Done With Day43

---
+++ Day43/main.py
@@ -0,0 +1,12 @@
+# RoadMap:
+    # https://whimsical.com/machine-learning-roadmap-by-ayush-singh-newera-J1EwnqAPUtF77ejgbRc8Hk
+    
+    
+# ML Resource:
+    # https://wheat-production-d58.notion.site/ML002-e469d005b2b14d5ba6f8c0bdfd305ded
+    
+    
+# Ds RoadMap:
+    # https://github.com/ayush714/data-science-roadmap?tab=readme-ov-file
+    
+    
