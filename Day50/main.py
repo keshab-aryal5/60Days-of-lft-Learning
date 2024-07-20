7310273df36b78a4f5a9abe3039adb9ea6c8d918
keshab Aryal <keshabaryal152@gmail.com>
2024-07-20 22:22:16 +0545

    Done with Day50

---
+++ Day50/main.py
@@ -0,0 +1,20 @@
+# Dictionaries: Key-value pair.
+# mydict = {"Name":"Max","Age":28,"City":"New York"}
+# print(mydict)
+
+
+# mydict2 = dict(name="Marry",age=27,city="Arghkhanci")
+# print(mydict2)
+
+# values = mydict2.values()
+# print(values)
+
+# mydict2 = dict(name="Marry",age=27,city="Arghkhanci")
+# # 
+# # value = mydict2["lastname"]
+# # print(value)
+# mydict2["email"] = "keshabaryal152@gmail.com"
+
+# print(mydict2)
+# del mydict2["email"]
+# print(mydict2)
